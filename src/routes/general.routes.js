9263f66c3c6ea18da95057b2be395320575f7b1e
AndresJejen <gajcam@gmail.com>
2019-01-25 21:08:58 -0500

    vertificado agregado y configurado

--- src/routes/general.routes.js
+++ src/routes/general.routes.js
@@ -14,8 +14,8 @@ router.get('/:otro',(req,res)=>{
     res.status(404).send({mensaje:`oops! Recurso \'${req.params.otro}\' No encontrado`})
 })
 
-router.get('/.well-known/acme-challenge/fUtXU59Y2-fjUHKpN5RPlyZDuS6VYxIhc4J9o4iBbWs',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../../well-known/acme-challenge/fUtXU59Y2-fjUHKpN5RPlyZDuS6VYxIhc4J9o4iBbWs'))
+router.get('/.well-known/acme-challenge/pZoInhYKZJTsrYA0JiFqUvelesxUXM4tiklfd2AMhtg',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../../well-known/acme-challenge/pZoInhYKZJTsrYA0JiFqUvelesxUXM4tiklfd2AMhtg'))
 })
 
 
